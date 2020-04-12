2b0fe211c26ddcf808cfb6af3318cea61d297a44
Cola2333 <594545815@qq.com>
2020-04-12 01:24:49 -0700

    init repository

---
+++ src/test/java/life/usc/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package life.usc.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
