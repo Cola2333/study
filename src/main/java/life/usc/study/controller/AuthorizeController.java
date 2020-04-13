61689d31a7c75646d41c6266bc92ebd820207e8b
cola2333 <junjiewa@usc.edu>
2020-04-12 22:55:02 -0700

    improv session

--- src/main/java/life/usc/study/controller/AuthorizeController.java
+++ src/main/java/life/usc/study/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -36,7 +38,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
                            @RequestParam("state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -49,11 +52,12 @@ public class AuthorizeController {
             User user = new User();
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             user.setName(gitHubUser.getName());
-            user.setToken(UUID.randomUUID().toString());//是javaJDK提供的一个自动生成主键的方法。UUID(Universally Unique Identifier)全局唯一标识符,是指在一台机器上生成的数字，它保证对在同一时空中的所有机器都是唯一的，是由一个十六位的数字组成,表现出来的 形式。由以下几部分的组合
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);//是javaJDK提供的一个自动生成主键的方法。UUID(Universally Unique Identifier)全局唯一标识符,是指在一台机器上生成的数字，它保证对在同一时空中的所有机器都是唯一的，是由一个十六位的数字组成,表现出来的 形式。由以下几部分的组合
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            userMapper.insert(user);
-            request.getSession().setAttribute("user", gitHubUser);
+            userMapper.insert(user); //相当于写入session
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         }else {
             return "redirect:/";
--- src/main/java/life/usc/study/mapper/UserMapper.java
+++ src/main/java/life/usc/study/mapper/UserMapper.java
@@ -3,9 +3,15 @@ package life.usc.study.mapper;
 import life.usc.study.moel.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
 
 @Mapper
 public interface UserMapper {
     @Insert("INSERT INTO USER(account_id,name,token,gmt_create,gmt_modified) VALUES(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
