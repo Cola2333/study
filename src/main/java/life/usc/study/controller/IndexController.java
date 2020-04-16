0c6aa85bc471bf5ccd66843e5d4b8b754718f368
cola2333 <junjiewa@usc.edu>
2020-04-15 21:59:27 -0700

    add interceptor

--- src/main/java/life/usc/study/controller/IndexController.java
+++ src/main/java/life/usc/study/controller/IndexController.java
@@ -1,28 +1,17 @@
 package life.usc.study.controller;
 
 import life.usc.study.dto.PaginationDTO;
-import life.usc.study.dto.QuestionDTO;
-import life.usc.study.mapper.UserMapper;
-import life.usc.study.moel.User;
 import life.usc.study.service.QuestionService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
@@ -32,21 +21,6 @@ public class IndexController {
                         Model model,
                         @RequestParam(name = "pageNum", defaultValue = "1") Integer pageNum,
                         @RequestParam(name = "size", defaultValue = "2") Integer size) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    //System.out.println(token);
-                    User user = userMapper.findByToken(token);
-                    //System.out.println(user);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
 
         PaginationDTO pagination = questionService.list(pageNum, size);
         model.addAttribute("questionList", pagination); //其实应该叫name应该是pagination 懒得改了
